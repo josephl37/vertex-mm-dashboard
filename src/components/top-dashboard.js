d7ca32bf3354dbe5134fe0fd4acab40c8271023d
josephl37 <josephliow2000@gmail.com>
2023-04-27 22:33:01 -0500

    update tooltips

--- src/components/top-dashboard.js
+++ src/components/top-dashboard.js
@@ -14,7 +14,7 @@ function TopDashboard() {
             src={infoSvg}
             alt="info"
             data-tooltip-id="my-tooltip"
-            data-tooltip-html="An epoch lasts for 28 days.<br />There are a total of 6 epochs."
+            data-tooltip-html="An epoch lasts for 28 days.<br />There are a total of 6 epochs<br />in the Vertex Trading Incentive<br/> Program (VTIP)."
           />
         </div>
         <div className="flex justify-center mt-4 mb-6">
@@ -28,7 +28,7 @@ function TopDashboard() {
             src={infoSvg}
             alt="info"
             data-tooltip-id="my-tooltip"
-            data-tooltip-html="A relative measure of how much $VRTX<br />is earned for a constant level of liquidity<br />(a constant depth and spread). A value of<br />1.00 is average. To mazimize rewards per<br />liquidity provided, one would provide<br />liquidity into the markets with the highest<br />reward coefficients."
+            data-tooltip-html="A dynamic, relative measure of how much<br /> $VRTX is earned for a constant level of<br />liquidity (a constant depth and spread). A<br />value of 1.00 is average. To mazimize<br />rewards per liquidity provided, one would<br />provide liquidity into the markets with the<br />highest reward coefficient."
           />
         </div>
         <div className="flex justify-center mt-4 mb-6">
@@ -44,7 +44,7 @@ function TopDashboard() {
             src={infoSvg}
             alt="info"
             data-tooltip-id="my-tooltip"
-            data-tooltip-html="The total amount of market makers<br />for each market in the current epoch."
+            data-tooltip-html="The total number of market makers<br />for each market in the current epoch."
           />
         </div>
         <div className="flex justify-center mt-4 mb-6">
