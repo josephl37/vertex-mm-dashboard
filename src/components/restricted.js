cea35eb60b9089d2902b801f9627bf53f8dd1e4c
josephl37 <josephliow2000@gmail.com>
2023-05-02 15:49:31 -0500

    added TOS link

--- src/components/restricted.js
+++ src/components/restricted.js
@@ -10,7 +10,7 @@ const Restricted = () => {
         <p className="text-white text-center text-4xl font-bold">Notice</p>
         <div className="mt-8 flex justify-center">
           <p className="text-white text-center whitespace-normal">
-          Unfortunately, we are not able to support users from restricted territories at this time. Please refer to the Vertex Terms of Service for more information.
+          Unfortunately, we are not able to support users from restricted territories at this time. Please refer to the <a href="https://vertexprotocol.com/docs/TermsOfService.pdf" target="_blank" rel="noreferrer" className="underline hover:text-gray-1">Vertex Terms of Service</a> for more information.
           </p>
         </div>
       </div>
